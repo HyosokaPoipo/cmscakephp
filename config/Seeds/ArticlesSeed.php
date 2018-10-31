c996bd061af984234c46e4674c923516804535c6
maryadi <maryadipoipo187@gmail.com>
2018-10-31 10:00:37 +0700

    add articles seeder

---
+++ config/Seeds/ArticlesSeed.php
@@ -0,0 +1,36 @@
+<?php
+use Migrations\AbstractSeed;
+use Cake\I18n\Time;
+
+/**
+ * Articles seed.
+ */
+class ArticlesSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            'user_id' => 1,
+            'title' => 'my first post',
+            'slug' => 'my-first-post',
+            'body' => 'this is the body of my first post',
+            'published' => true,
+            'created' => Time::now(),
+            'modified' => Time::now()git 
+
+        ];
+
+        $table = $this->table('articles');
+        $table->insert($data)->save();
+    }
+}
